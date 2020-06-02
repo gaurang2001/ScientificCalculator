5e688bfb48037db28e2c5b90c8781cea8cf455dc
abhid2001 <54524628+abhid2001@users.noreply.github.com>
2020-06-02 15:46:02 +0530

    Top Row Functionality - resolved (#8)
    * Top Row Functionality
    
    * Changes

--- app/src/main/java/com/example/scientificcalculator/MainActivity.java
+++ app/src/main/java/com/example/scientificcalculator/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Initialise variables
     TextView textView, textView2, textView3;
-    Button b1, b2, b3, b4, b5, b6, b7, b8, b9, b0, b_dot, b_equal,b_sign, btn, b_plus, b_minus, b_multiply, b_divide,b_backspace,b_percentage, b_log10, b_ln, b_root, b_xfac, b_sin, b_cos, b_tan, b_exponent;
+    Button b1, b2, b3, b4, b5, b6, b7, b8, b9, b0, b_dot, b_equal,b_sign, btn, b_plus, b_minus, b_multiply, b_divide,b_backspace,b_percentage, b_log10, b_ln, b_root, b_xfac, b_sin, b_cos, b_tan,b_asin,b_acos,b_atan,b_ex, b_exponent;
     float result; // Stores number in textView
     float result2; // Used when arithmetic or scientific functions are called
     boolean Add, Sub, Mul, Div, exp; // Stores truth value of function call
@@ -49,6 +49,10 @@ public class MainActivity extends AppCompatActivity {
         b_sin = findViewById(R.id.b_sin);
         b_cos = findViewById(R.id.b_cos);
         b_tan = findViewById(R.id.b_tan);
+        b_asin = findViewById(R.id.b_asin);
+        b_acos = findViewById(R.id.b_acos);
+        b_atan = findViewById(R.id.b_atan);
+        b_ex = findViewById(R.id.b_ex);
         b_exponent = findViewById(R.id.b_exponent);
         textView = findViewById(R.id.textView);
         textView2 = findViewById(R.id.textView2);
@@ -419,6 +423,55 @@ public class MainActivity extends AppCompatActivity {
                 textView3.setText("tan("+degrees+") = "+result);
             }
         });
+
+        b_asin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float val = Float.parseFloat(textView.getText() + "");
+                result = (float) Math.asin(val);
+                result = (float) Math.toDegrees(result);
+                textView.setText("" + result);
+                textView3.setText("sin-1("+val+") = "+result);
+
+            }
+        });
+
+        b_acos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float val = Float.parseFloat(textView.getText() + "");
+                result = (float) Math.acos(val);
+                result = (float) Math.toDegrees(result);
+                textView.setText("" + result);
+                textView3.setText("cos-1("+val+") = "+result);
+
+            }
+        });
+
+        b_atan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float val = Float.parseFloat(textView.getText() + "");
+                result = (float) Math.atan(val);
+                result = (float) Math.toDegrees(result);
+                textView.setText("" + result);
+                textView3.setText("tan-1("+val+") = "+result);
+
+            }
+        });
+
+        b_ex.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result = Float.parseFloat(textView.getText()+ "");
+                result = (float) Math.exp(result);
+                textView2.setText("");
+                textView.setText(result+"");
+                textView3.setText("e^x"+"");
+            }
+        });
+
+
 
         // Performs power function
         b_exponent.setOnClickListener(new View.OnClickListener() {
